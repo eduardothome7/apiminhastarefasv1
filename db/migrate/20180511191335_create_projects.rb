ea5b304530f870cc48bef4679af17bdf4ba738f5
eduardo thomé <eduardocanninithome@hotmail.com>
2018-05-18 10:05:38 -0300

    setup project

--- db/migrate/20180511191335_create_projects.rb
+++ db/migrate/20180511191335_create_projects.rb
@@ -3,7 +3,8 @@ class CreateProjects < ActiveRecord::Migration[5.1]
     create_table :projects do |t|
       t.string :name
       t.text :description
-
+      t.integer :team_id
+      
       t.timestamps
     end
   end
--- db/migrate/20180511195033_add_estimate_min_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddEstimateMinToTasks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :tasks, :estimate_min, :decimal
-  end
-end
