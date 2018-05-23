ab52afab225e0ba201db9321ae4e13be020d7b87
eduardo thomé <eduardocanninithome@hotmail.com>
2018-05-23 15:40:18 -0300

    setup before filter

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,5 @@
 class StatusesController < ApplicationController
   before_action :set_status, only: [:show, :edit, :update, :destroy]
-  skip_before_action :verify_authenticity_token
 
   # GET /statuses
   # GET /statuses.json
